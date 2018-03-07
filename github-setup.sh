396c9487e40170661c735577e01a7d794c21c58b
acduroy <acduroy@yahoo.com>
2018-03-07 14:09:20 -0800

    github-setup revision 2

--- github-setup.sh
+++ github-setup.sh
@@ -1,31 +1,30 @@
-#! /bin/bash
+#!/bin/bash
+
 clear
 echo -e "\e[3J"	#disable scroll
 
 usage(){
 	clear
-	echo "repositoriy name was not supplied"
 	echo "Usage $0 github-setup"
-	echo "Ex: $ bridge-networking"
+	echo "repositoriy name was not supplied"
+	echo "Ex: ./github-setup.sh <name of the repository>"
 	exit 1
 }
 
+# check if repository name is supplied in the command line #
+# if not call usage() function #
+[[ $# -eq 0 ]] && usage
 
+# install git package #
+sudo apt-get install git -y
 
 # git configuration setup #
 git config --global user.name "acduroy"
 git config --global user.email "acduroy@yahoo.com"
 git config --global color.ui auto
 
-# call usage() if directory name is not supplied #
-[[ $# -eq 0 ]] && usage
-
-
-# create repositories #
-mkdir $1
-cd $1
-git init 
+# create repositories in home directory #
+cd $PWD
 git clone https://github.com/acduroy/$1.git
 
 exit 0
-
