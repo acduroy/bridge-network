cefad992fde526005eab8f55bd5309032f2057ff
acduroy <acduroy@yahoo.com>
2018-03-22 18:30:43 -0700

    revise nat-enablement

--- nat-enable-only.sh
+++ nat-enable-only.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y iptables-persistent
 sudo iptables -t nat -A POSTROUTING -o $EXT -j MASQUERADE  
 sudo iptables -A FORWARD -i $EXT -o $INT -m state --state RELATED,ESTABLISHED -j ACCEPT  
 sudo iptables -A FORWARD -i $INT -o $EXT -j ACCEPT  
-#echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf  
-#sudo sysctl -p
+echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf  
+sudo sysctl -p
 sudo iptables-save | sudo tee /etc/iptables/rules.v4
 
